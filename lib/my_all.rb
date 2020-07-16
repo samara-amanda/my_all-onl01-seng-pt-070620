02495e8a12ed775b37d91429fcf6dc1fc2b167c1
Samara Grant <samara.amanda01@gmail.com>
2020-07-16 01:04:23 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,7 +3,9 @@ require 'pry'
 def my_all?(collection)
   i = 0
   block_return_values = []
+
   while i < collection.length
+    binding.py
     block_return_values << yield(collection[i])
     i = i + 1
   else
